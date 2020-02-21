363f94fcb509482ef877e36606c949720931f463
mereddyHarshitha <mereddy.harshitha@gmail.com>
2020-02-21 11:25:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+var nforce = require('nforce');
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
 const PORT = process.env.PORT || 5000
+
+var org = nforce.createConnection({
+  clientId: '3MVG9n_HvETGhr3AerhMPQWA3Z_X_pZduM18nC6jICZEY3ZAJynYZSnga4b3rMWiNgtj6II4of3kcEe.vgLL9',
+  clientSecret: '28DD8FEB6824FBFFA664936E73A0359EACE498F595BC7DCCF0D7B352677F6974',
+  redirectUri: 'https://lwcopplist.herokuapp.com/oauth/_callback',
+  apiVersion: 'v47.0',  // optional, defaults to current salesforce API version
+  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
+});
+var routesHome = require('./routes/home');
 
 express()
   .use(function(req, res, next) {
@@ -12,7 +23,20 @@ express()
       res.setHeader('Access-Control-Max-Age', '1000');
       next();
   })
+  .use('/home', routesHome);
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', cors(), (req, res) => res.render('pages/index'))
+  .get('/', cors(), (req, res) => res.redirect(org.getAuthUri())
+  .get('/oauth/_callback', function(req, res) {
+  org.authenticate({code: req.query.code}, function(err, resp){
+    if(!err) {
+      console.log('Access Token: ' + resp.access_token);
+      app.locals.oauthtoken = resp.access_token;
+      app.locals.lightningEndPointURI = "https://hmereddydemocomporg-dev-ed.lightning.force.com";
+      res.redirect('/home');
+    } else {
+      console.log('Error: ' + err.message);
+    }
+  });
+})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
