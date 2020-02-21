e9a33d3825cfbf96072b2483029e7865d2338e2a
mereddyHarshitha <mereddy.harshitha@gmail.com>
2020-02-21 11:28:02 -0500

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-	    res.render('pages/home', {
+	    res.render('pages/index', {
 	        oauthtoken: req.app.locals.oauthtoken,
 	        ouathLightningURL: req.app.locals.lightningEndPointURI
     });
